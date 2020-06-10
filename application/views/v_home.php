82237688164c15e25881c981a91f340f55674b2d
moesaldi <31431648+moesaldi@users.noreply.github.com>
2020-06-10 13:51:04 +0700

    update

--- application/views/v_home.php
+++ application/views/v_home.php
@@ -41,9 +41,9 @@
                             <div class="card" style="width: 18rem;">
                               <img class="card-img-top" style="width: 286px;height: 180px;" src="<?=base_url('/assets/img/'.$key->gambar)?>">
                               <div class="card-body">
-                                <h5 class="card-title"><?= $key->nama_resep ?></h5>
-                                <p class="card-text"><?= $key->deskripsi ?></p>
-                                <a href="#" class="btn success">Go somewhere</a>
+                                <h5 class="card-title font-weight-bold"><?= $key->nama_resep ?></h5>
+                                <p class="card-text"><?= $key->deskripsi_singkat ?></p>
+                                <a href="<?=base_url('index.php/resep/lihatResep/'.$key->id_resep)?>" class="btn success">Lihat Resep</a>
                               </div>
                             </div>
                         </div>
@@ -73,8 +73,8 @@
                             <div class="card" style="width: 18rem;">
                               <img class="card-img-top" style="width: 286px;height: 180px;" src="<?=base_url('/assets/img/'.$key->gambar)?>">
                               <div class="card-body">
-                                <h5 class="card-title"><?= $key->nama_resep ?></h5>
-                                <p class="card-text"><?= $key->deskripsi ?></p>
+                                <h5 class="card-title font-weight-bold"><?= $key->nama_resep ?></h5>
+                                <p class="card-text"><?= $key->deskripsi_singkat ?></p>
                                 <a href="<?=base_url('index.php/resep/lihatResep/'.$key->id_resep)?>" class="btn success">Lihat Resep</a>
                               </div>
                             </div>
@@ -106,9 +106,9 @@
                             <div class="card" style="width: 18rem;">
                               <img class="card-img-top" style="width: 286px;height: 180px;" src="<?=base_url('/assets/img/'.$key->gambar)?>">
                               <div class="card-body">
-                                <h5 class="card-title"><?= $key->nama_resep ?></h5>
-                                <p class="card-text"><?= $key->deskripsi ?></p>
-                                <a href="#" class="btn success">Go somewhere</a>
+                                <h5 class="card-title font-weight-bold"><?= $key->nama_resep ?></h5>
+                                <p class="card-text"><?= $key->deskripsi_singkat ?></p>
+                                <a href="<?=base_url('index.php/resep/lihatResep/'.$key->id_resep)?>" class="btn success">Lihat Resep</a>
                               </div>
                             </div>
                         </div>
--- application/views/v_resep.php
+++ application/views/v_resep.php
@@ -2,27 +2,29 @@
 <section class="page-section" id="rekomendasi" style="background: url('<?= base_url()?>/assets/img/bg-login.jpg');background-position: center; background-repeat: no-repeat;background-size: cover;padding-top: 10rem;
   padding-bottom: calc(10rem - 4.5rem);">
             <div class="container" style="background-color: #fff; border-radius: 20px;">
-                <h2 class="text-center mt-0 font-weight-bold" style="padding-top: 25px;"><?= $detailResep->nama_resep?></h2>
+                <h1 class="text-center mt-0 font-weight-bold" style="padding-top: 25px;"><?= $detailResep->nama_resep?></h1>
                 <hr class="divider my-4" />
-                <div class="row justify-content-center">
-                    <?php
-                    foreach ($data_resep as $key):
-                        
-                    
-                    ?>
-                        <div class="col-sm text-center" style="align-items: center;">
-                            <div class="card" style="width: 18rem;">
-                              <img class="card-img-top" style="width: 286px;height: 180px; border-radius: 15px;" src="<?=base_url('/assets/img/'.$key->gambar)?>">
-                              <div class="card-body">
-                                <h5 class="card-title"><?= $key->nama_resep ?></h5>
-                                <p class="card-text"><?= $key->deskripsi ?></p>
-                                <a href="<?=base_url('index.php/resep/lihatResep/'.$key->id_resep)?>" class="btn success">Lihat Resep</a>
-                              </div>
-                            </div>
-                        </div>
-                    <?php
-                    endforeach
-                    ?>
+                <div class="row" style="padding: 15px;">
+                    <div class="col-8 text-center"><h5><?= $detailResep->deskripsi?></h5></div>
+                    <div class="col-4"><img style="width: 300px; border-radius: 15px;" src="<?=base_url('assets/img/'.$detailResep->gambar )?>"></div>
+                </div>
+                <h2 style="padding-left: 15px;padding-top: 20px;" class="font-weight-bold">Alat & Bahan</h2>
+                <hr>
+                <div class="row" style="padding: 15px;">
+                    <div class="col-8"><h5><?= $detailResep->ingridient?></h5>
+                    </div>
+                </div>
+                <h2 style="padding-left: 15px;padding-top: 20px;" class="font-weight-bold">Petunjuk Pembuatan</h2>
+                <hr>
+                <div class="row" style="padding: 15px;">
+                    <div class="col-12"><h5><?= $detailResep->petunjuk_pembuatan?></h5>
+                    </div>
+                </div>
+                <div class="row " style="padding: 15px;padding-bottom: 50px;">
+                    <div class=" col-4">
+                        <h6 style="padding-top: 20px;" class="font-weight-bold">Beri Rating Pada Resep Ini</h6>
+                        <img style="width: 200px;" src="<?=base_url()?>/assets/img/rating.png"><a class="btn btn-success" href="#">Rate</a>
+                    </div>
                 </div>
             </div>
         </section>
